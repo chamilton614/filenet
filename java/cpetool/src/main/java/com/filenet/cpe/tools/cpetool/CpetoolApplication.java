441bcd9ec9c4bb81f872582cde3688403195f152
Chad J. Hamilton <>
2021-08-08 12:41:13 -0400

    Baseline

---
+++ java/cpetool/src/main/java/com/filenet/cpe/tools/cpetool/CpetoolApplication.java
@@ -0,0 +1,13 @@
+package com.filenet.cpe.tools.cpetool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpetoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpetoolApplication.class, args);
+	}
+
+}
---
+++ java/cpetool/src/test/java/com/filenet/cpe/tools/cpetool/CpetoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filenet.cpe.tools.cpetool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpetoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
