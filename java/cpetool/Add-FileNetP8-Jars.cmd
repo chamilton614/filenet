36740ad88b905b8697b71e3283e04c364ced1114
Chad J. Hamilton <>
2021-08-19 11:53:21 -0400

    Updated to use wrapper and windows mvnw

--- java/cpetool/Add-FileNetP8-Jars.cmd
+++ java/cpetool/Add-FileNetP8-Jars.cmd
@@ -20,103 +20,90 @@ REM Add to the local maven repository
 set GroupId=com.filenet.p8.pe
 set JarParentDir=PEEventExportAPI
 set JarName=eeapi
-./mvnw install:install-file -Dfile=%FileNetP8JarsRoot%/%FileNetP8Version%/%JarParentDir%/%JarName%.jar -DgroupId=%GroupId% -DartifactId=%JarName% -Dversion=%FileNetP8Version% -Dpackaging=jar
+mvnw install:install-file -Dfile=%FileNetP8JarsRoot%/%FileNetP8Version%/%JarParentDir%/%JarName%.jar -DgroupId=%GroupId% -DartifactId=%JarName% -Dversion=%FileNetP8Version% -Dpackaging=jar
 
 REM Add to the local maven repository
 set GroupId=com.filenet.p8.ce
 set JarParentDir=JavaCEWSclient
 set JarName=isannotvalidator
-./mvnw install:install-file -Dfile=%FileNetP8JarsRoot%/%FileNetP8Version%/%JarParentDir%/%JarName%.jar -DgroupId=%GroupId% -DartifactId=%JarName% -Dversion=%FileNetP8Version% -Dpackaging=jar
+mvnw install:install-file -Dfile=%FileNetP8JarsRoot%/%FileNetP8Version%/%JarParentDir%/%JarName%.jar -DgroupId=%GroupId% -DartifactId=%JarName% -Dversion=%FileNetP8Version% -Dpackaging=jar
 
 REM Add to the local maven repository
 set GroupId=com.filenet.p8.ce
 set JarParentDir=JavaCEWSclient
 set JarName=Jace
-./mvnw install:install-file -Dfile=%FileNetP8JarsRoot%/%FileNetP8Version%/%JarParentDir%/%JarName%.jar -DgroupId=%GroupId% -DartifactId=%JarName% -Dversion=%FileNetP8Version% -Dpackaging=jar
+mvnw install:install-file -Dfile=%FileNetP8JarsRoot%/%FileNetP8Version%/%JarParentDir%/%JarName%.jar -DgroupId=%GroupId% -DartifactId=%JarName% -Dversion=%FileNetP8Version% -Dpackaging=jar
 
 REM Add to the local maven repository
 set GroupId=com.filenet.p8.ce
 set JarParentDir=JavaCompatibilityLayer64bit
 set JarName=javaapi
-./mvnw install:install-file -Dfile=%FileNetP8JarsRoot%/%FileNetP8Version%/%JarParentDir%/%JarName%.jar -DgroupId=%GroupId% -DartifactId=%JarName% -Dversion=%FileNetP8Version% -Dpackaging=jar
+mvnw install:install-file -Dfile=%FileNetP8JarsRoot%/%FileNetP8Version%/%JarParentDir%/%JarName%.jar -DgroupId=%GroupId% -DartifactId=%JarName% -Dversion=%FileNetP8Version% -Dpackaging=jar
 
 REM Add to the local maven repository
 set GroupId=com.filenet.p8.ce
 set JarParentDir=JavaCompatibilityLayer64bit
 set JarName=listener
-./mvnw install:install-file -Dfile=%FileNetP8JarsRoot%/%FileNetP8Version%/%JarParentDir%/%JarName%.jar -DgroupId=%GroupId% -DartifactId=%JarName% -Dversion=%FileNetP8Version% -Dpackaging=jar
+mvnw install:install-file -Dfile=%FileNetP8JarsRoot%/%FileNetP8Version%/%JarParentDir%/%JarName%.jar -DgroupId=%GroupId% -DartifactId=%JarName% -Dversion=%FileNetP8Version% -Dpackaging=jar
 
 REM Add to the local maven repository
 set GroupId=com.filenet.p8.ce
 set JarParentDir=JavaCEWSclient
 set JarName=log4j-1.2.14
-./mvnw install:install-file -Dfile=%FileNetP8JarsRoot%/%FileNetP8Version%/%JarParentDir%/%JarName%.jar -DgroupId=%GroupId% -DartifactId=%JarName% -Dversion=%FileNetP8Version% -Dpackaging=jar
+mvnw install:install-file -Dfile=%FileNetP8JarsRoot%/%FileNetP8Version%/%JarParentDir%/%JarName%.jar -DgroupId=%GroupId% -DartifactId=%JarName% -Dversion=%FileNetP8Version% -Dpackaging=jar
 
 REM Add to the local maven repository
 set GroupId=com.filenet.p8.ce
 set JarParentDir=JavaCEWSclient
 set JarName=p8cel10n
-./mvnw install:install-file -Dfile=%FileNetP8JarsRoot%/%FileNetP8Version%/%JarParentDir%/%JarName%.jar -DgroupId=%GroupId% -DartifactId=%JarName% -Dversion=%FileNetP8Version% -Dpackaging=jar
+mvnw install:install-file -Dfile=%FileNetP8JarsRoot%/%FileNetP8Version%/%JarParentDir%/%JarName%.jar -DgroupId=%GroupId% -DartifactId=%JarName% -Dversion=%FileNetP8Version% -Dpackaging=jar
 
 REM Add to the local maven repository
 set GroupId=com.filenet.p8.ce
 set JarParentDir=JavaCompatibilityLayer64bit
 set JarName=p8cjares
-./mvnw install:install-file -Dfile=%FileNetP8JarsRoot%/%FileNetP8Version%/%JarParentDir%/%JarName%.jar -DgroupId=%GroupId% -DartifactId=%JarName% -Dversion=%FileNetP8Version% -Dpackaging=jar
+mvnw install:install-file -Dfile=%FileNetP8JarsRoot%/%FileNetP8Version%/%JarParentDir%/%JarName%.jar -DgroupId=%GroupId% -DartifactId=%JarName% -Dversion=%FileNetP8Version% -Dpackaging=jar
 
 REM Add to the local maven repository
 set GroupId=com.filenet.p8.pe
 set JarParentDir=PEJavaAPI
 set JarName=pe
-./mvnw install:install-file -Dfile=%FileNetP8JarsRoot%/%FileNetP8Version%/%JarParentDir%/%JarName%.jar -DgroupId=%GroupId% -DartifactId=%JarName% -Dversion=%FileNetP8Version% -Dpackaging=jar
+mvnw install:install-file -Dfile=%FileNetP8JarsRoot%/%FileNetP8Version%/%JarParentDir%/%JarName%.jar -DgroupId=%GroupId% -DartifactId=%JarName% -Dversion=%FileNetP8Version% -Dpackaging=jar
 
 REM Add to the local maven repository
 set GroupId=com.filenet.p8.pe
 set JarParentDir=PEJavaAPI
 set JarName=pe3pt.jar
-./mvnw install:install-file -Dfile=%FileNetP8JarsRoot%/%FileNetP8Version%/%JarParentDir%/%JarName%.jar -DgroupId=%GroupId% -DartifactId=%JarName% -Dversion=%FileNetP8Version% -Dpackaging=jar
+mvnw install:install-file -Dfile=%FileNetP8JarsRoot%/%FileNetP8Version%/%JarParentDir%/%JarName%.jar -DgroupId=%GroupId% -DartifactId=%JarName% -Dversion=%FileNetP8Version% -Dpackaging=jar
 
 REM Add to the local maven repository
 set GroupId=com.filenet.p8.pe
 set JarParentDir=PEJavaAPI
 set JarName=peResources
-./mvnw install:install-file -Dfile=%FileNetP8JarsRoot%/%FileNetP8Version%/%JarParentDir%/%JarName%.jar -DgroupId=%GroupId% -DartifactId=%JarName% -Dversion=%FileNetP8Version% -Dpackaging=jar
+mvnw install:install-file -Dfile=%FileNetP8JarsRoot%/%FileNetP8Version%/%JarParentDir%/%JarName%.jar -DgroupId=%GroupId% -DartifactId=%JarName% -Dversion=%FileNetP8Version% -Dpackaging=jar
 
 REM Add to the local maven repository
 set GroupId=com.filenet.p8.pe
 set JarParentDir=PERESTAPI
 set JarName=peREST
-./mvnw install:install-file -Dfile=%FileNetP8JarsRoot%/%FileNetP8Version%/%JarParentDir%/%JarName%.jar -DgroupId=%GroupId% -DartifactId=%JarName% -Dversion=%FileNetP8Version% -Dpackaging=jar
+mvnw install:install-file -Dfile=%FileNetP8JarsRoot%/%FileNetP8Version%/%JarParentDir%/%JarName%.jar -DgroupId=%GroupId% -DartifactId=%JarName% -Dversion=%FileNetP8Version% -Dpackaging=jar
 
 REM Add to the local maven repository
 set GroupId=com.filenet.p8.ce
 set JarParentDir=JavaCEWSclient
 set JarName=stax-api
-./mvnw install:install-file -Dfile=%FileNetP8JarsRoot%/%FileNetP8Version%/%JarParentDir%/%JarName%.jar -DgroupId=%GroupId% -DartifactId=%JarName% -Dversion=%FileNetP8Version% -Dpackaging=jar
+mvnw install:install-file -Dfile=%FileNetP8JarsRoot%/%FileNetP8Version%/%JarParentDir%/%JarName%.jar -DgroupId=%GroupId% -DartifactId=%JarName% -Dversion=%FileNetP8Version% -Dpackaging=jar
 
 REM Add to the local maven repository
 set GroupId=com.filenet.p8.ce
 set JarParentDir=JavaCEWSclient
 set JarName=xlxpScanner
-./mvnw install:install-file -Dfile=%FileNetP8JarsRoot%/%FileNetP8Version%/%JarParentDir%/%JarName%.jar -DgroupId=%GroupId% -DartifactId=%JarName% -Dversion=%FileNetP8Version% -Dpackaging=jar
+mvnw install:install-file -Dfile=%FileNetP8JarsRoot%/%FileNetP8Version%/%JarParentDir%/%JarName%.jar -DgroupId=%GroupId% -DartifactId=%JarName% -Dversion=%FileNetP8Version% -Dpackaging=jar
 
 REM Add to the local maven repository
 set GroupId=com.filenet.p8.ce
 set JarParentDir=JavaCEWSclient
 set JarName=xlxpScannerUtils
-./mvnw install:install-file -Dfile=%FileNetP8JarsRoot%/%FileNetP8Version%/%JarParentDir%/%JarName%.jar -DgroupId=%GroupId% -DartifactId=%JarName% -Dversion=%FileNetP8Version% -Dpackaging=jar
-
-
-
-
-
-
-
-
-
-
-
-
-
+mvnw install:install-file -Dfile=%FileNetP8JarsRoot%/%FileNetP8Version%/%JarParentDir%/%JarName%.jar -DgroupId=%GroupId% -DartifactId=%JarName% -Dversion=%FileNetP8Version% -Dpackaging=jar
 
 
